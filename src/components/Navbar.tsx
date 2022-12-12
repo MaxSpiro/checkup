c666305c03d341b92b1e6d3d7681cad481bf3669
MaxSpiro <maxspiridonov04@gmail.com>
2022-12-11 19:57:11 -0800

    new nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,59 +7,132 @@ import { Fragment } from "react";
 export const Navbar: React.FC = () => {
   const { data: session } = useSession();
   return (
-    <header className="bg-violet-300">
-      <nav>
-        <ul className="flex items-center gap-6 py-2 px-6 text-xl text-black">
-          <li className="mr-auto text-4xl hover:underline">
-            <Link href="/">Checkup</Link>
-          </li>
-          <li className="hover:underline">
-            <Link href="/about">About</Link>
-          </li>
-          {session?.user?.image ? (
-            <Menu as="li" className="relative inline-block text-left">
-              <div>
-                <Menu.Button className="relative h-16 w-16 overflow-hidden rounded-full">
-                  {" "}
-                  <Image
-                    src={session.user?.image}
-                    alt="user profile"
-                    fill={true}
-                  />
-                </Menu.Button>
-              </div>
-              <Transition
-                as={Fragment}
-                enter="transition ease-out duration-100"
-                enterFrom="transform opacity-0 scale-95"
-                enterTo="transform opacity-100 scale-100"
-                leave="transition ease-in duration-75"
-                leaveFrom="transform opacity-100 scale-100"
-                leaveTo="transform opacity-0 scale-95"
-              >
-                <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md ring-1">
-                  <div className="px-1 py-1">
-                    <Menu.Item>
-                      {({ active }) => (
-                        <button
-                          className={`flex w-full items-center rounded-md p-2 ${
-                            active ? "bg-violet-600 text-slate-100" : ""
-                          }`}
-                          onClick={() => signOut()}
-                        >
-                          Log out
-                        </button>
-                      )}
-                    </Menu.Item>
-                  </div>
-                </Menu.Items>
-              </Transition>
-            </Menu>
-          ) : (
-            <button onClick={() => signIn()}>Sign in</button>
-          )}
-        </ul>
-      </nav>
-    </header>
+    <nav className="px-6 py-4 bg-slate-300 lg:flex lg:items-center lg:justify-between">
+      <div className="min-w-0 flex-1">
+        <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+          Back End Developer
+        </h2>
+      </div>
+      <div className="mt-5 flex lg:mt-0 lg:ml-4">
+        <span className="hidden sm:block">
+          <button
+            type="button"
+            className="inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            <svg
+              className="-ml-1 mr-2 h-5 w-5 text-gray-500"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path d="M2.695 14.763l-1.262 3.154a.5.5 0 00.65.65l3.155-1.262a4 4 0 001.343-.885L17.5 5.5a2.121 2.121 0 00-3-3L3.58 13.42a4 4 0 00-.885 1.343z" />
+            </svg>
+            Edit
+          </button>
+        </span>
+
+        <span className="ml-3 hidden sm:block">
+          <button
+            type="button"
+            className="inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            <svg
+              className="-ml-1 mr-2 h-5 w-5 text-gray-500"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path d="M12.232 4.232a2.5 2.5 0 013.536 3.536l-1.225 1.224a.75.75 0 001.061 1.06l1.224-1.224a4 4 0 00-5.656-5.656l-3 3a4 4 0 00.225 5.865.75.75 0 00.977-1.138 2.5 2.5 0 01-.142-3.667l3-3z" />
+              <path d="M11.603 7.963a.75.75 0 00-.977 1.138 2.5 2.5 0 01.142 3.667l-3 3a2.5 2.5 0 01-3.536-3.536l1.225-1.224a.75.75 0 00-1.061-1.06l-1.224 1.224a4 4 0 105.656 5.656l3-3a4 4 0 00-.225-5.865z" />
+            </svg>
+            View
+          </button>
+        </span>
+
+        <span className="sm:ml-3">
+          <button
+            type="button"
+            className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            <svg
+              className="-ml-1 mr-2 h-5 w-5"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            Publish
+          </button>
+        </span>
+
+        <div className="relative ml-3 sm:hidden">
+          <button
+            type="button"
+            className="inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            id="mobile-menu-button"
+            aria-expanded="false"
+            aria-haspopup="true"
+          >
+            More
+            <svg
+              className="-mr-1 ml-2 h-5 w-5 text-gray-500"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </button>
+
+          {/* Dropdown menu, show/hide based on menu state.
+
+        Entering: "transition ease-out duration-200"
+          From: "transform opacity-0 scale-95"
+          To: "transform opacity-100 scale-100"
+        Leaving: "transition ease-in duration-75"
+          From: "transform opacity-100 scale-100"
+          To: "transform opacity-0 scale-95" */}
+          <div
+            className="absolute right-0 z-10 mt-2 -mr-1 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+            role="menu"
+            aria-orientation="vertical"
+            aria-labelledby="mobile-menu-button"
+            tabIndex={-1}
+          >
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700"
+              role="menuitem"
+              tabIndex={-1}
+              id="mobile-menu-item-0"
+            >
+              Edit
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700"
+              role="menuitem"
+              tabIndex={-1}
+              id="mobile-menu-item-1"
+            >
+              View
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 };
